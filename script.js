a5ad9ec8df762d5b700987e26e04296d52bffcbc
Bex <rgonza01@risd.edu>
2019-01-28 12:09:35 -0400

    Dropdown Menus working

---
+++ script.js
@@ -0,0 +1,303 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function yearFunction() {
+  document.getElementById("yearDropdown").classList.toggle("show");
+}
+function yearFunction01() {
+  document.getElementById("yearDropdown01").classList.toggle("show");
+}
+function yearFunction02() {
+  document.getElementById("yearDropdown02").classList.toggle("show");
+}
+function yearFunction03() {
+  document.getElementById("yearDropdown03").classList.toggle("show");
+}
+function yearFunction04() {
+  document.getElementById("yearDropdown04").classList.toggle("show");
+}
+function yearFunction05() {
+  document.getElementById("yearDropdown05").classList.toggle("show");
+}
+function yearFunction06() {
+  document.getElementById("yearDropdown06").classList.toggle("show");
+}
+function yearFunction07() {
+  document.getElementById("yearDropdown07").classList.toggle("show");
+}
+function yearFunction08() {
+  document.getElementById("yearDropdown08").classList.toggle("show");
+}
+function yearFunction09() {
+  document.getElementById("yearDropdown09").classList.toggle("show");
+}
+function yearFunction10() {
+  document.getElementById("yearDropdown10").classList.toggle("show");
+}
+function yearFunction11() {
+  document.getElementById("yearDropdown11").classList.toggle("show");
+}
+function yearFunction12() {
+  document.getElementById("yearDropdown12").classList.toggle("show");
+}
+function yearFunction13() {
+  document.getElementById("yearDropdown13").classList.toggle("show");
+}
+function yearFunction14() {
+  document.getElementById("yearDropdown14").classList.toggle("show");
+}
+function yearFunction15() {
+  document.getElementById("yearDropdown15").classList.toggle("show");
+}
+function yearFunction16() {
+  document.getElementById("yearDropdown16").classList.toggle("show");
+}
+function yearFunction17() {
+  document.getElementById("yearDropdown17").classList.toggle("show");
+}
+function yearFunction18() {
+  document.getElementById("yearDropdown18").classList.toggle("show");
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.button')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn01')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn02')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn03')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn04')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn05')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn06')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn07')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn08')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn09')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn10')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn11')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn12')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn13')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn14')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn15')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn16')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn17')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.yearbtn18')) {
+      var yeardropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = yeardropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
